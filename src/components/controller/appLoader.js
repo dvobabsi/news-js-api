09ee1495c8005fa7a42efca4b0cec0106e5fe97b
dvobabsi <dvobabsi@gmail.com>
2025-04-25 16:29:19 +0300

    feat: add tsconfig

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -2,10 +2,17 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(process.env.API_URL, {
-            apiKey: process.env.API_KEY,
+        super('https://newsapi.org/v2/', {
+            apiKey: '7a110283b19a45a283e6a82e7a27b8c9',
         });
     }
 }
+// class AppLoader extends Loader {
+//     constructor() {
+//         super(process.env.API_URL, {
+//             apiKey: process.env.API_KEY,
+//         });
+//     }
+// }
 
 export default AppLoader;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
+// const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const DotenvWebpackPlugin = require('dotenv-webpack');
+// const EslintWebpackPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
+            { test: /\.ts$/i, use: 'ts-loader' },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
@@ -16,19 +18,22 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-        new DotenvWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
+        // new webpack.DefinePlugin({
+        //     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+        // }),
         new CleanWebpackPlugin(),
+        // new EslintWebpackPlugin({ extensions: 'ts' }),
     ],
 };
 
